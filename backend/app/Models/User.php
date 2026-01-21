8cf7c0b384a053d753de21943bd4653ad5185af7
KenBel <kenzabelaloui6@gmail.com>
2026-01-21 18:29:27 +0100

    feat(reviews): add reviews model and schema

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -48,7 +48,8 @@ class User extends Authenticatable
 
     public function reviews()
     {
-        return $this->hasMany(Review::class);
+        return $this->hasMany(\App\Models\Review::class);
     }
+
 
 }
